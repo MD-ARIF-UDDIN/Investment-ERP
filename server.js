dee675fda1a96da919f705343b864402fbc91d5f
arif.uddin <arif.uddin@eunoiaits.com>
2026-04-20 16:57:40 +0600

    sas

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
     res.send('স্বপ্নের বাতিঘর এপিআই ইজ রানিং...'); // স্বপ্নের বাতিঘর API is running...
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
